edd2c7b11d818dfee2bf433ead10333704120e43
yiqin <yiqin.mems@gmail.com>
2014-10-22 18:14:56 -0500

    Add PFQueryTableViewController

---
+++ Voice/NewsTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  NewsTableViewController.swift
+//  Voice
+//
+//  Created by yiqin on 10/22/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewController: PFQueryTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func objectsDidLoad(error: NSError!) {
+        super.objectsDidLoad(error)
+    }
+    
+    
+    override func objectsWillLoad() {
+        super.objectsWillLoad()
+    }
+    
+    override func queryForTable() -> PFQuery! {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
