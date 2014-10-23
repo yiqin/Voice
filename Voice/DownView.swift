357fc4c03061d7fc1f5665c5bdef9b0425600fad
yiqin <yiqin.mems@gmail.com>
2014-10-23 18:35:18 -0500

    Add a UI Test. Need more design details

--- Voice/DownView.swift
+++ Voice/DownView.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class DownView: UIView {
 
+    // For testing
+    var articlesTableVC = ArticlesTableViewController(style: UITableViewStyle.Plain)
+    
     func updateFrame(minY:CGFloat){
         backgroundColor = UIColor.grayColor()
         frame = CGRectMake(0, minY, DeviceManager.sharedInstance.screenWidth, DeviceManager.sharedInstance.screenHeight-minY)
+        self.addSubview(articlesTableVC.view)
+        loadArticlesTableVC(minY)
     }
     
+    func loadArticlesTableVC(minY:CGFloat) {
+        articlesTableVC.view.frame = CGRectMake(0, 0, DeviceManager.sharedInstance.screenWidth, DeviceManager.sharedInstance.screenHeight-minY)
+    }
     
     
     
