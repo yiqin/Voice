d05db5f1a44e6ef3b7bf7bd21c2e078f50a2d1cc
yiqin <yiqin.mems@gmail.com>
2015-01-06 12:02:04 -0600

    The new design with the pre - new....

---
+++ Voice/NewMainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewMainViewController.swift
+//  Voice
+//
+//  Created by Yi Qin on 1/6/15.
+//  Copyright (c) 2015 yiqin. All rights reserved.
+//
+
+import Cocoa
+
+class NewMainViewController: UIViewController {
+
+}
