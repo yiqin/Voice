d448b3464ddf2e3b0e17aa78ed73d02988ac15e4
yiqin <yiqin.mems@gmail.com>
2015-01-26 14:36:12 -0600

    Add Back Button in streetView.

--- Voice/StreetDetailViewController.swift
+++ Voice/StreetDetailViewController.swift
@@ -12,9 +12,15 @@ class StreetDetailViewController: UIViewController {
     
     var streetDetailTVC : StreetDetailBodyTableViewController
     
+    var showBackSubView : Bool = false
+    var backSubView : UIView
+    let backOffset : CGFloat = 44
+    
     init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?, article selectedStreetImage: StreetImage? ) {
         
         streetDetailTVC = StreetDetailBodyTableViewController(selectedStreetImage: selectedStreetImage!)
+        backSubView = UIView(frame: CGRectMake(0, -backOffset, DeviceManager.sharedInstance.screenWidth, backOffset))
+        
         
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         self.view.backgroundColor = UIColor.whiteColor()
@@ -25,8 +31,12 @@ class StreetDetailViewController: UIViewController {
         view.addSubview(streetDetailTVC.view)
         
         
-        var swipeRight = UISwipeGestureRecognizer(target: self, action: "swipeRight:")
-        self.view.addGestureRecognizer(swipeRight)
+        backSubView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.8)
+        
+        
+        var tempBackButton = YQButtonWithImage(frame: CGRectMake(6, 4, 36, 36), image: "back", selectedImage: "back")
+        backSubView.addSubview(tempBackButton)
+        tempBackButton.addTarget(self, action: "backButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
     }
 
     required init(coder aDecoder: NSCoder) {
@@ -39,12 +49,18 @@ class StreetDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "swipeRight:")
+        view.addGestureRecognizer(swipeRight)
+        
+        var tapGestureRecognizer = UITapGestureRecognizer(target: self, action: "tapScreen:")
+        view.addGestureRecognizer(tapGestureRecognizer)
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        
         
     }
 
@@ -54,7 +70,10 @@ class StreetDetailViewController: UIViewController {
     }
     
     func backButtonPressed(sender: UIButton!){
-        self.navigationController?.popViewControllerAnimated(true)
+        // self.navigationController?.popViewControllerAnimated(true)
+        dismissViewControllerAnimated(true, completion: { () -> Void in
+            
+        })
     }
 
     func swipeRight(recognizer:UISwipeGestureRecognizer){
@@ -63,6 +82,34 @@ class StreetDetailViewController: UIViewController {
         dismissViewControllerAnimated(true, completion: { () -> Void in
             
         })
+    }
+    
+    func tapScreen(recognizer:UISwipeGestureRecognizer){
+        println("Tap... .")
+        
+        // showBackSubView = true
+        checkBackSubView()
+    }
+    
+    func checkBackSubView(){
+        if (showBackSubView){
+            UIView.animateWithDuration(0.5, delay: 0.0, options: .CurveEaseOut, animations: { () -> Void in
+                self.backSubView.frame = CGRectMake(0, -self.backOffset, DeviceManager.sharedInstance.screenWidth, self.backOffset)
+                
+                }, completion: { (finish) -> Void in
+                    self.showBackSubView = false
+            })
+            
+        }
+        else {
+            view.addSubview(backSubView)
+            UIView.animateWithDuration(0.5, delay: 0.0, options: .CurveEaseOut, animations: { () -> Void in
+                self.backSubView.frame = CGRectMake(0, 0, DeviceManager.sharedInstance.screenWidth, self.backOffset)
+                
+                }, completion: { (finish) -> Void in
+                    self.showBackSubView = true
+            })
+        }
     }
     
     /*
