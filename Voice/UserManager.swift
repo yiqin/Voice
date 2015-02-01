9a2d529b95ed0e12f5504209f8d8000512dd4561
yiqin <yiqin.mems@gmail.com>
2015-01-31 23:34:51 -0600

    Review code and clean code.

--- Voice/UserManager.swift
+++ Voice/UserManager.swift
@@ -21,27 +21,25 @@ class UserManager: NSObject {
         return Static.instance
     }
     
+    /// Setup General, all iOS users are General type now.
     class func configureRole(userType : String) {
         var tempQuery = PFQuery(className: "_Role")
         tempQuery.whereKey("name", equalTo: userType)
         tempQuery.findObjectsInBackgroundWithBlock { (objects:[AnyObject]!, error:NSError!) -> Void in
             // It supports to be only one return object.
-            println("helllooooooooooooo")
-            println(objects.count)
             for object in objects {
                 var tempRole: PFRole = object as PFRole
                 println(tempRole["name"])
                 var userRelation = tempRole.relationForKey("users")
                 userRelation.addObject(PFUser.currentUser())
                 tempRole.saveInBackgroundWithBlock({ (successed : Bool!, error : NSError!) -> Void in
-                    println("Something woong")
                     if((error) != nil){
+                        println("Something woong")
                         println(error)
                     }
                 })
             }
         }
-
     }
     
 }
