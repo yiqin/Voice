451baf42b9d678afda6bb0a3f6e1ce7ce137a70a
yiqin <yiqin.mems@gmail.com>
2015-02-18 11:13:36 -0600

    Add RGCardViewLayout

---
+++ Voice/RGCardViewLayout.h
@@ -0,0 +1,13 @@
+//
+//  RGCardViewLayout.h
+//  Voice
+//
+//  Created by Yi Qin on 2/18/15.
+//  Copyright (c) 2015 yiqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGCardViewLayout : UICollectionViewFlowLayout
+
+@end
