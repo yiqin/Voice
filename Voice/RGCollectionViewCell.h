05ec418beaad8b2175ee1a1ded5787f5792db560
yiqin <yiqin.mems@gmail.com>
2015-02-18 11:24:15 -0600

    Add RGCollectionViewCell

---
+++ Voice/RGCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  RGCollectionViewCell.h
+//  Voice
+//
+//  Created by Yi Qin on 2/18/15.
+//  Copyright (c) 2015 yiqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UILabel *mainLabel;
+
+@end
---
+++ Voice/RGCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  RGCollectionViewCell.m
+//  Voice
+//
+//  Created by Yi Qin on 2/18/15.
+//  Copyright (c) 2015 yiqin. All rights reserved.
+//
+
+#import "RGCollectionViewCell.h"
+
+@implementation RGCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
