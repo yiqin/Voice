33ad6244074dffb5d0886e98e78d3a917138098b
yiqin <yiqin.mems@gmail.com>
2015-03-05 18:36:54 -0600

    Fixed the launching bug.

--- Voice/LaunchingViewController.swift
+++ Voice/LaunchingViewController.swift
@@ -22,53 +22,43 @@ class LaunchingViewController: UIViewController, UIWebViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reloadSessionStreetImageTableViewCell", name: "reloadSessionStreetImageTableViewCell", object: nil)
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(true)
-        
-        view.backgroundColor = UIColor(red: 35.0/255.0, green: 24.0/255.0, blue: 21.0/255.0, alpha: 1.0)
-        
         let tempScreenWidth = UIScreen.mainScreen().bounds.width
         let tempScreenHeight = UIScreen.mainScreen().bounds.height
         let ratio = tempScreenHeight/tempScreenWidth
         
         
         gifImageView = UIImageView(frame: CGRectMake(0, 0, tempScreenWidth, tempScreenHeight))
-        /*
-        if (tempScreenHeight/tempScreenWidth >= 960.0/640.0) {
-            let tempHeight = tempScreenWidth*960/640
-            gifImageView.frame = CGRectMake(0, (view.frame.height-tempHeight)*0.5, view.frame.width, tempHeight)
-        }
-        */
         gifImageView.frame = CGRectMake(0, 0, tempScreenWidth, tempScreenHeight)
         gifImageView.contentMode = UIViewContentMode.ScaleAspectFill
         gifImageView.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
-        gifImageView.backgroundColor = UIColor.whiteColor()
+        gifImageView.backgroundColor = UIColor.clearColor()
         view.addSubview(gifImageView)
         
         pngImageView = UIImageView(frame: CGRectMake(0, 0, tempScreenWidth, tempScreenHeight))
         pngImageView.contentMode = UIViewContentMode.ScaleAspectFill
         pngImageView.autoresizingMask = UIViewAutoresizing.FlexibleWidth |  UIViewAutoresizing.FlexibleHeight
         pngImageView.image = UIImage(named: "coming_soon_background")
+        pngImageView.backgroundColor = UIColor.clearColor()
         view.insertSubview(pngImageView, belowSubview: gifImageView)
         
         // Always get launching_1.gif
         /*
         if(VoiceUserDefaultsDataManager.sharedInstance.checkFirstTimeLoad()){
-            gifImageView.setAnimatableImage(data: VoiceUserDefaultsDataManager.sharedInstance.getLaunchingGIF())
+        gifImageView.setAnimatableImage(data: VoiceUserDefaultsDataManager.sharedInstance.getLaunchingGIF())
         }
         else {
-            gifImageView.setAnimatableImage(named: "launching_1.gif")
+        gifImageView.setAnimatableImage(named: "launching_1.gif")
         }
         */
         
         gifImageView.setAnimatableImage(named: "coming_soon.gif")
         gifImageView.startAnimating()
         
-        println(view.frame.size.width)
-        println(view.frame.size.height)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reloadSessionStreetImageTableViewCell", name: "reloadSessionStreetImageTableViewCell", object: nil)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
         
     }
     
